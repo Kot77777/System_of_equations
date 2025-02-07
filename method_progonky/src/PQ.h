184adb49c4475e2aa97e7f0f6ba6d9aab52c5739
kostya <tanov.kl@phystech.edu>
2025-02-07 11:43:41 +0300

    correct_PQ.h

--- method_progonky/src/PQ.h
+++ method_progonky/src/PQ.h
@@ -3,10 +3,8 @@
 
 template<typename T>
 struct PQ{
-	T p_;
-	T q_;
-	PQ(const T p, const T q) : p_(p), q_(q){}
-	PQ() : p_(0), q_(0) {}
+	T p_ = 0;
+	T q_ = 0;
 };
 
 #endif //METHOD_PROGONKY_PQX_H
