e7d308462d596b1f5ab95718818cc874af02718d
kostya <tanov.kl@phystech.edu>
2025-04-19 22:41:16 +0300

    method_CG

---
+++ src/solution_SLAE/method_ConjugateGradients.h
@@ -0,0 +1,31 @@
+#ifndef solution_SLAE_METHOD_CONJUGATE_GRADIENT_H
+#define solution_SLAE_METHOD_CONJUGATE_GRADIENT_H
+#include <utility>
+
+#include "primitives/vector_from_vector.h"
+#include "primitives/CSR_matrix.h"
+
+template<typename T>
+Vector<T> method_ConjugateGradients(const CSR_matrix<T> &A, const Vector<T> &b, const Vector<T> &x_0, const std::size_t N_iter, const T eps) {
+    Vector<T> r_i = A * x_0 - b;
+    Vector<T> r_i_next{r_i};
+    Vector<T> d_i{r_i};
+    Vector<T> x_i{x_0};
+    T alpha_i, betta_i{};
+    std::size_t n_iter{};
+
+    while (r_i.norm() >= eps && n_iter < N_iter) {
+        alpha_i = dot(r_i, r_i) / dot(d_i, A * d_i);
+        x_i = x_i - alpha_i * d_i;
+        r_i_next = A * x_i - b;
+        betta_i = dot(r_i_next, r_i_next) / dot(r_i, r_i);
+        d_i = r_i_next + betta_i * d_i;
+        r_i = r_i_next;
+
+        n_iter += 1;
+    }
+
+    return x_i;
+}
+
+#endif //solution_SLAE_METHOD_CONJUGATE_GRADIENT_H
---
+++ tests/test_method_ConjugateGradients.cpp
@@ -0,0 +1,32 @@
+#include <gtest/gtest.h>
+
+#include "solution_SLAE/method_ConjugateGradients.h"
+
+TEST(method_ConjugateGradients, method_ConjugateGradients) {
+    std::map<std::array<std::size_t, 2>, double> DOK;
+
+    DOK[{0, 0}] = 6.0;
+    DOK[{1, 1}] = 5.0;
+    DOK[{2, 2}] = 4.0;
+    DOK[{3, 3}] = 3.0;
+    DOK[{4, 4}] = 4.0;
+    DOK[{0, 1}] = 2.0;
+    DOK[{1, 0}] = 2.0;
+    DOK[{0, 2}] = 1.0;
+    DOK[{2, 0}] = 1.0;
+    DOK[{1, 2}] = 1.0;
+    DOK[{2, 1}] = 1.0;
+    DOK[{1, 3}] = 1.0;
+    DOK[{3, 1}] = 1.0;
+    DOK[{2, 4}] = 1.0;
+    DOK[{4, 2}] = 1.0;
+    DOK[{3, 4}] = 2.0;
+    DOK[{4, 3}] = 2.0;
+
+    const CSR_matrix<double> A{DOK, 5, 5};
+    const Vector<double> b{{1., 2., 4., 8., 10.}, 5};
+    const double eps = 1e-15;
+    const Vector<double> x_0{5};
+
+    const Vector<double> res = method_ConjugateGradients(A, b, x_0, 1000, eps);
+}
